b9012c047f581119073fbcf0c927941a07f4f263
maxBRT <bourret74@gmail.com>
2025-12-17 12:09:11 -0500

    Scaffold database_manager

---
+++ README.md
@@ -0,0 +1,5 @@
+# Todo
+
+- [ ] Scaffold database_manager methods
+- [ ] Scaffold pages (Clients, Equipments, Locations) 
+- [ ] Scaffold MainWindow and navigation
--- database_manager.py
+++ database_manager.py
@@ -1,3 +1,50 @@
+from database import SessionLocal
+
+
 class DatabaseManager:
     def __init__(self):
+        pass
+
+    def create_client(self, name, email, phone):
+        pass
+
+    def create_equipment(self, name, cost_per_day, is_available):
+        pass
+
+    def create_location(
+        self, id_client, id_equipment, start_date, end_date, is_returned
+    ):
+        pass
+
+    def get_clients(self):
+        pass
+
+    def get_equipments(self):
+        pass
+
+    def get_available_equipments(self):
+        pass
+
+    def get_locations(self):
+        pass
+
+    def get_client_by_id(self, id):
+        pass
+
+    def get_equipment_by_id(self, id):
+        pass
+
+    def get_location_by_id(self, id):
+        pass
+
+    def get_client_by_phone(self, phone):
+        pass
+
+    def get_equipment_by_name(self, name):
+        pass
+
+    def get_location_by_client_id(self, id_client):
+        pass
+
+    def get_location_by_equipment_name(self, id_equipment):
         pass
